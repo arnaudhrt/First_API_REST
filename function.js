ea2e23812197594d82fb5d9328dff626a93c5c87
Arnaud <arnaud3488@gmail.com>
2021-11-11 22:47:33 +0100

    Ajout Base de donée mysql

--- function.js
+++ function.js
@@ -10,3 +10,15 @@ export function error (message) {
      result: message
   }
 }
+export function success (result) {
+  return {
+     status: 'success',
+     result: result
+  }
+}
+export function error (message) {
+  return {
+     status: 'error',
+     result: message
+  }
+}
